7f1a862d229336ac27d5122063c8842a82a88101
Rian Lima <101999658+CaarlosRiian@users.noreply.github.com>
2023-11-24 14:29:07 -0300

    ainda não terminei

--- hospedagem/admin.py
+++ hospedagem/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .views import HospedagemCriar, HospedagemDetalhe, HospedagemEditar, HospedagemRemover, HospedagemListar
 
-# Register your models here.
+@admin.register(HospedagemCriar)
+class HospedagemAdmin(admin.ModelAdmin):
+    list_display=('','endereco','email')
+
+@admin.register(HospedagemDetalhe)
+class HospedagemAdmin(admin.ModelAdmin):
+    list_display=('nome',)
+
+@admin.register(HospedagemEditar)
+class HospedagemAdmin(admin.ModelAdmin):
+    list_display=('nome','sigla_estado',)
+
+@admin.register(HospedagemRemover)
+class HospedagemAdmin(admin.ModelAdmin):
+    list_display=('nome','sigla_estado',)
+
+@admin.register(HospedagemListar)
+class HospedagemAdmin(admin.ModelAdmin):
+    list_display=('nome','sigla_estado',)
+
--- hospedagem/models.py
+++ hospedagem/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
-    nome = models.ForeignKey(max_length=100)
+    nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     telefone = models.CharField(max_length=50)
     endereco = models.CharField(max_length=150)
 
 class Quarto(models.Model):
-    apartamento = models.IntegerField(max_length=200)
+    apartamento = models.IntegerField()
     valor = models.FloatField(max_length=200)
 
 class Hospedagem(models.Model):
@@ -23,3 +23,5 @@ class Consumo(models.Model):
     data = models.DateField(max_length=100)
     valor = models.FloatField(max_length=100)
     hospedagem = models.ForeignKey(Hospedagem, on_delete=models.CASCADE)
+
+# Models feitos!
